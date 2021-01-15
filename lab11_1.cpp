c5fee38b8fccff36563ed973cea534049ff9a6f2
DarkTXYZ <pawaretmontfort@gmail.com>
2021-01-15 15:12:28 +0700

    Corrected

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
-    cout << fibonacci(x);
+    cout << fibonacci(x) << "\n";
     return 0; 
+}
+
+int fibonacci(int r){
+    if(r == 0)
+        return 0;
+    if(r == 1)
+        return 1;
+    return fibonacci(r-1) + fibonacci(r-2);
 }
