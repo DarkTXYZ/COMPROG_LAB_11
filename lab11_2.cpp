b237c728071bbbfb88d5f8f4616ab7159f485f2e
DarkTXYZ <pawaretmontfort@gmail.com>
2021-01-15 15:18:03 +0700

    Update

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int a , int b){
+    if(a < b)
+        return gcd(b,a);
+    if(a % b == 0)
+        return b;
+    return gcd(b , a%b);
 }
